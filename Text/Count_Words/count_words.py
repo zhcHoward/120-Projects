7d327f2b20bcc991fa2d9c99aa640d61e04cb881
Howard Zhu <zhc_howard@hotmail.com>
2016-09-19 14:01:45 +0800

    modify gitignore, finish yesterday's project

--- Text/Count_Words/count_words.py
+++ Text/Count_Words/count_words.py
@@ -3,11 +3,10 @@
 
 """
 Usage:
-    count_words [--option]... [filename]
-    count_words -f FILENAME, --file=FILENAME
+    count_words [options] <content>...
 
 Options:
-    -f FILENAME, --file FILENAME    Read string from file.
+    -f, --file                      Read string from file.
     -h, --help                      Show this screen.
     --version                       Show version.
 """
@@ -18,7 +17,23 @@ from docopt import docopt
 # Counts the number of individual words in a string. For added complexity read
 # these strings in from a text file and generate a summary.
 
+def count_words(string):
+    word_list = string.split()
+    return len(word_list)
+
+
 if __name__ == '__main__':
     args = docopt(__doc__, version='1.0')
     print(args)
+
+    if args['--file']:
+        word_num = 0
+        for filename in args['<content>']:
+            with open(filename, encoding='utf-8') as reader:
+                for line in reader:
+                    word_num += count_words(line)
+    else:
+        word_num = len(args['<content>'])
+
+    print(word_num)
 
